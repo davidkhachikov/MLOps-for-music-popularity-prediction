77efda52e400c53926e4d3f3761025379a9edeea
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2024-06-21 20:45:41 +0300

    fixed test for checking types in 'streams' column

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -13,7 +13,7 @@ def test_is_not_empty():
 def test_streams_type():
     """test for checking correctness of types in 'streams' column"""
     for val in data["streams"]:
-        assert isinstance(val, np.int32, np.nan)
+        assert isinstance(val, np.int32, type(np.nan))
 
 def test_artist_followers_type():
     """test for checking correctness of types in 'artist_followers' column"""
